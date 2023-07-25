f8dbb3de43438cd747502e45223ca9804ccf3a94
aditya11900 <adityaraj153566@gmail.com>
2023-07-25 18:13:34 +0530

    comments

--- Patterns/halfPyramid.cpp
+++ Patterns/halfPyramid.cpp
@@ -4,8 +4,8 @@ int main(){
 
 int n;
 cin>>n;
-for(int row=0; row<n; row++){
-    for(int col=0; col<row+1; col++){
+for(int row=1; row<=n; row++){
+    for(int col=1; col<=row; col++){ // if col = 0 then you have to increase it to col<row+1;
         cout<<"*";
     }
     cout<<endl;
