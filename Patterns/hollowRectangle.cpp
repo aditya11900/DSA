37a4c3e6bb9bc6444b759a850ea17bdbff87bfe7
aditya11900 <adityaraj153566@gmail.com>
2023-07-25 18:04:07 +0530

    more problems on Patterns

---
+++ Patterns/hollowRectangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rowCount, colCount;
+    cin>>rowCount>>colCount;
+    for(int row=0; row<rowCount; row++){
+        if(row==0 || row==rowCount-1){
+            for(int col =0; col<colCount; col++){
+                cout<<"*";
+            }
+        }else{
+            cout<<"*";
+            for(int i=0; i<colCount-2;i++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
