42e421f30fc303c21ae4c081f5befe1a6005cd70
aditya11900 <adityaraj153566@gmail.com>
2023-08-10 14:12:10 +0530

    with even natural numbers

--- functions/printSumOfNNumbers.cpp
+++ functions/printSumOfNNumbers.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 using namespace std;
+
 int SumOfNumbers(int n){
     int sum = 0;
-    for(int i=0;i<n;i++){
+    for(int i = 0; i < n; i++){
         sum = sum + i;
     }
     return sum;
 }
+
+int evenNaturalNumbers(int n){
+    int sum = 0;
+    for(int i = 2; i <= n; i = i + 2){
+        sum = sum + i;
+    }
+    return sum;
+}
+
 int main(){
     int n;
-    cout<<"enter n till you want to print sum:"<<endl;
-    cin>>n;
-    int result = SumOfNumbers(n);
-    cout<<result;
+    cout << "Enter n till you want to calculate the sum: " << endl;
+    cin >> n;
+
+    int naturalSum = evenNaturalNumbers(n);
+    int totalSum = SumOfNumbers(n);
+
+    cout << "Sum of numbers from 0 to " << n - 1 << ": " << totalSum << endl;
+    cout << "Sum of even natural numbers from 2 to " << n << ": " << naturalSum << endl;
+
+    return 0;
 }
